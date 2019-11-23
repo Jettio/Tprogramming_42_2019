bf7ec0aef83be6555a9bc0bcd21baa42f81f8b14
Jettio <contrakt120@gmail.com>
2019-11-23 14:41:35 +0300

    1st lab

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -1,24 +1,45 @@
-using System;
 using Xunit;
 
 namespace CourseApp.Tests
 {
     public class DemoTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 2, 2, -1.939)]
+        [InlineData(1, 2, 5, 2.936)]
+        public void TestEquat(double a, double b, double x, double exp)
         {
-            Assert.True(true);
+            Assert.Equal(Program.EquatFunction(a, b, x), exp, 3);
         }
 
-        [Theory]
-        [InlineData(0, 0, 0, 0)]
-        [InlineData(0, 2, 1, 2)]
-        [InlineData(1, 2, 1, 3)]
-        public void TestFunctionCalculationVal(double a, double b, double x, double exp)
+        [Fact]
+        public void TestTaskAArray0()
         {
-            var res = Program.MyFunction(a, b, x);
-            Assert.Equal(exp, res, 3);
+            var res = Program.TaskA(0.8, 0.4, 1.23, 0.23, 1.2);
+            Assert.Equal(res, new double[0]);
+        }
+
+        [Fact]
+        public void TestTaskAArray1()
+        {
+            var res = Program.TaskA(0.8, 0.4, 1.23, 7.23, 6).Length;
+            Assert.Equal(1, res);
+        }
+
+        [Fact]
+        public void TestTaskBArray0()
+        {
+            double[] e = new double[0];
+            var res = Program.TaskB(0.8, 0.4, e);
+            Assert.Equal(res, new double[0]);
+        }
+
+        [Fact]
+        public void TestTaskBWork()
+        {
+            double[] mass = new double[5] { 1.88, 2.26, 3.84, 4.55, -6.21 };
+            var res = Program.TaskB(0.8, 0.4, mass);
+            Assert.Equal(5, res.Length);
         }
     }
 }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,43 +4,57 @@ namespace CourseApp
 {
     public class Program
     {
-        public static double MyFunction(double a, double b, double x)
+        public static double EquatFunction(double a, double b, double x)
         {
-            var y = (a * Math.Pow(x, 2)) + (b * x);
-            return y;
+            return (Math.Pow(Math.Pow(x - a, 2), 1.0 / 3.0) + Math.Pow(Math.Abs(x + b), 1.0 / 5.0)) / Math.Pow(Math.Pow(x, 2) - Math.Pow(a + b, 2), 1.0 / 9.0);
         }
 
         public static double[] TaskA(double a, double b, double xn, double xk, double dx)
         {
-            return new double[0];
-        }
-
-        public static double[] TaskB(double a, double b, double[] x)
-        {
-            var y = new double[x.Length];
-            for (var i = 0; i < x.Length; i++)
+            int size = (int)Math.Floor((xk - xn) / dx);
+            double[] rtrn = new double[size + 1];
+            size = 0;
+            for (double x = xn; x < (xk + 0.1); x += dx)
             {
-                y[i] = MyFunction(a, b, x[i]);
+                rtrn[size] = EquatFunction(a, b, x);
+                size++;
             }
 
-            return y;
+            return rtrn;
+        }
+
+        public static double[] TaskB(double a, double b, double[] xm)
+        {
+            double[] rtrn = new double[xm.Length];
+            for (int i = 0; i < xm.Length; i++)
+            {
+                rtrn[i] = EquatFunction(a, b, xm[i]);
+            }
+
+            return rtrn;
         }
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            const double a = 2.2;
-            const double b = 3.8;
-            var resSingle = MyFunction(a, b, 4);
-            Console.WriteLine(resSingle);
-            var x = new double[] { 1, 2, 3, 4, 5 };
-            var taskBRes = TaskB(a, b, x);
-            foreach (var item in taskBRes)
+            double a = 0.8;
+            double b = 0.4;
+            double[] mass = TaskA(a, b, 1.23, 7.23, 1.2);
+            foreach (var item in mass)
             {
-                Console.WriteLine($"y = {item}");
+                Console.WriteLine(item);
             }
 
-            Console.ReadLine();
+            double[] mass2 = new double[5] { 1.88, 2.26, 3.84, 4.55, -6.21 };
+            mass2 = TaskB(a, b, mass2);
+            foreach (var item in mass2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("------------");
+            Console.WriteLine(EquatFunction(1.0, 2.0, 5));
+
+            Console.ReadKey();
         }
     }
 }
