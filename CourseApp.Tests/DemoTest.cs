fae0b463863d5a3647d6f9d3e71c62a8b4be6b44
Jettio <contrakt120@gmail.com>
2020-01-03 17:56:38 +0300

    Lists

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace CourseApp.Tests
@@ -9,37 +11,44 @@ namespace CourseApp.Tests
         [InlineData(1, 2, 5, 2.936)]
         public void TestEquat(double a, double b, double x, double exp)
         {
-            Assert.Equal(Program.EquatFunction(a, b, x), exp, 3);
+            Assert.Equal(Program.Equat(a, b, x), exp, 3);
         }
 
         [Fact]
-        public void TestTaskAArray0()
+        public void TestTaskAempty()
         {
             var res = Program.TaskA(0.8, 0.4, 1.23, 0.23, 1.2);
-            Assert.Equal(res, new double[0]);
+            Assert.Equal(res, new List<double>());
         }
 
         [Fact]
-        public void TestTaskAArray1()
+        public void TestTaskA()
         {
-            var res = Program.TaskA(0.8, 0.4, 1.23, 7.23, 6).Length;
+            var res = Program.TaskA(0.8, 0.4, 1.23, 7.23, 6).Count;
+            Assert.Equal(2, res);
+        }
+
+        [Fact]
+        public void TestTaskA2()
+        {
+            var res = Program.TaskA(0.8, 0.4, 1.23, 7.23, 6).Count;
             Assert.Equal(1, res);
         }
 
         [Fact]
-        public void TestTaskBArray0()
+        public void TestTaskB()
         {
-            double[] e = new double[0];
+            List<double> e = new List<double>();
             var res = Program.TaskB(0.8, 0.4, e);
-            Assert.Equal(res, new double[0]);
+            Assert.Equal(res, new List<double>());
         }
 
         [Fact]
         public void TestTaskBWork()
         {
-            double[] mass = new double[5] { 1.88, 2.26, 3.84, 4.55, -6.21 };
-            var res = Program.TaskB(0.8, 0.4, mass);
-            Assert.Equal(5, res.Length);
+            List<double> arr = new List<double>() { 1.88, 2.26, 3.84, 4.55, -6.21 };
+            var res = Program.TaskB(0.8, 0.4, arr).Count;
+            Assert.Equal(5, res);
         }
     }
 }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,34 +1,32 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
 namespace CourseApp
 {
     public class Program
     {
-        public static double EquatFunction(double a, double b, double x)
+        public static double Equat(double a, double b, double x)
         {
             return (Math.Pow(Math.Pow(x - a, 2), 1.0 / 3.0) + Math.Pow(Math.Abs(x + b), 1.0 / 5.0)) / Math.Pow(Math.Pow(x, 2) - Math.Pow(a + b, 2), 1.0 / 9.0);
         }
 
-        public static double[] TaskA(double a, double b, double xn, double xk, double dx)
+        public static List<double> TaskA(double a, double b, double xn, double xk, double dx)
         {
-            int size = (int)Math.Floor((xk - xn) / dx);
-            double[] rtrn = new double[size + 1];
-            size = 0;
+            List<double> rtrn = new List<double>();
             for (double x = xn; x < (xk + 0.1); x += dx)
             {
-                rtrn[size] = EquatFunction(a, b, x);
-                size++;
+                rtrn.Add(Equat(a, b, x));
             }
 
             return rtrn;
         }
 
-        public static double[] TaskB(double a, double b, double[] xm)
+        public static List<double> TaskB(double a, double b, List<double> xm)
         {
-            double[] rtrn = new double[xm.Length];
-            for (int i = 0; i < xm.Length; i++)
+            List<double> rtrn = new List<double>();
+            foreach (double item in xm)
             {
-                rtrn[i] = EquatFunction(a, b, xm[i]);
+                rtrn.Add(Equat(a, b, item));
             }
 
             return rtrn;
@@ -38,13 +36,13 @@ namespace CourseApp
         {
             double a = 0.8;
             double b = 0.4;
-            double[] mass = TaskA(a, b, 1.23, 7.23, 1.2);
+            List<double> mass = TaskA(a, b, 1.23, 7.23, 1.2);
             foreach (var item in mass)
             {
                 Console.WriteLine(item);
             }
 
-            double[] mass2 = new double[5] { 1.88, 2.26, 3.84, 4.55, -6.21 };
+            List<double> mass2 = new List<double>() { 1.88, 2.26, 3.84, 4.55, -6.21 };
             mass2 = TaskB(a, b, mass2);
             foreach (var item in mass2)
             {
@@ -52,7 +50,7 @@ namespace CourseApp
             }
 
             Console.WriteLine("------------");
-            Console.WriteLine(EquatFunction(1.0, 2.0, 5));
+            Console.WriteLine(Equat(1.0, 2.0, 5));
 
             Console.ReadKey();
         }
